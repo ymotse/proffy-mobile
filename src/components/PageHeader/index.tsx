81a62ef9462c917aa10ae334788cd347fa0baf72
Yitshhaq Fukushima <ymotse@gmail.com>
2020-08-11 21:01:06 -0300

    Built teacher filter

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { View, Image, Text } from 'react-native'
 import { BorderlessButton } from 'react-native-gesture-handler'
 
@@ -11,11 +11,12 @@ import styles from './styles'
 
 
 interface PageHeaderProps {
-    title: string
+    title: string,
+    headerRight?: ReactNode
 }
 
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({ title, headerRight, children }) => {
     
     const { navigate } = useNavigation()
     
@@ -34,7 +35,13 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
                 <Image source={logoImg} resizeMode="contain" />
             </View>
             
-            <Text style={styles.title}>{title}</Text>
+            <View style={styles.header}>
+                <Text style={styles.title}>{title}</Text>
+                {headerRight}
+            </View>
+            
+            
+            {children}
         </View>
     )
 }
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import { ScrollView, View } from 'react-native'
+import React, { useState } from 'react'
+import { TextInput, Text, ScrollView, View } from 'react-native'
+import { BorderlessButton, RectButton } from 'react-native-gesture-handler'
+import { Feather } from '@expo/vector-icons'
 
 import PageHeader from '../../components/PageHeader'
 import TeacherItem from '../../components/TeacherItem'
@@ -8,9 +10,59 @@ import styles from './styles'
 
 
 function TeacherList() {
+    
+    const [isFiltersVisible, setIsFiltersVisible] = useState(false)
+    
+    
+    function handleToggleFiltersVisible() {
+        setIsFiltersVisible(!isFiltersVisible)
+    }
+    
     return (
         <View style={styles.container}>
-            <PageHeader title="Proffys disponíveis" />
+            <PageHeader 
+                title="Proffys disponíveis" 
+                headerRight={(
+                    <BorderlessButton onPress={handleToggleFiltersVisible}>
+                        <Feather name="filter" size={20} color="#fff" />
+                    </BorderlessButton>
+                )}
+            >
+                {isFiltersVisible && (
+                    <View style={styles.searchForm}>
+                        <Text style={styles.label}>Matéria</Text>
+                        <TextInput 
+                            style={styles.input}
+                            placeholder="Qual a matéria?" 
+                            placeholderTextColor="#c1bccc"
+                        />
+                        
+                        <View style={styles.inputGroup}>
+                            <View style={styles.inputBlock}>
+                                <Text style={styles.label}>Dia da semana</Text>
+                                <TextInput 
+                                    style={styles.input}
+                                    placeholder="Qual o dia?" 
+                                    placeholderTextColor="#c1bccc"
+                                />
+                            </View>
+                            
+                            <View style={styles.inputBlock}>
+                                <Text style={styles.label}>Horário</Text>
+                                <TextInput 
+                                    style={styles.input}
+                                    placeholder="Qual horário?" 
+                                    placeholderTextColor="#c1bccc"
+                                />
+                            </View>
+                        </View>
+                        
+                        <RectButton style={styles.submitButton}>
+                            <Text style={styles.submitButtonText}>Filtrar</Text>
+                        </RectButton>
+                    </View>
+                )}
+            </PageHeader>
             
             <ScrollView 
                 style={styles.teacherList}
