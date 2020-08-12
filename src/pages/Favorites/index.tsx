0b5f88a3bfcdd61a068d24d7d9688b8ce4045cd3
Yitshhaq Fukushima <ymotse@gmail.com>
2020-08-11 22:35:22 -0300

    List favorite teachers

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, ScrollView } from 'react-native'
+import AsyncStorage from '@react-native-community/async-storage'
+import { useFocusEffect } from '@react-navigation/native'
 
 import PageHeader from '../../components/PageHeader'
 import TeacherItem, { Teacher } from '../../components/TeacherItem'
@@ -8,6 +10,21 @@ import styles from './styles'
 
 
 function Favorites() {
+    
+    const [favorites, setFavorites] = useState([])
+    
+    function loadFavorites() {
+        AsyncStorage.getItem('favorites').then(response => { 
+            if(response) {
+                setFavorites(JSON.parse(response))
+            }
+        })
+    }
+    
+    useFocusEffect(() => {
+        loadFavorites()
+    })
+    
     return (
         <View style={styles.container}>
             <PageHeader title="Meus proffys favoritos" />
@@ -19,6 +36,16 @@ function Favorites() {
                     paddingBottom: 16
                 }}
             >
+                
+                {favorites.map((teacher: Teacher) => {
+                    return (
+                        <TeacherItem 
+                            key={teacher.id} 
+                            teacher={teacher}
+                            favorited
+                        />
+                    )
+                })}
                 
             </ScrollView>
         </View>
